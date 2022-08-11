21f163654c72d2581a35869b8a5a88320926f87a
behnam hesabi <atlasplants@gmail.com>
2022-08-11 09:30:14 +0430

    omid

---
+++ Assets/Script/ControllExplosionEffectBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class ControllExplosionEffectBullet : MonoBehaviour
+{
+
+    public void Start()
+    {
+        Destroy(this.gameObject, 2.0f);
+    }
+}
---
+++ Assets/Script/CrossHairControll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class CrossHairControll : MonoBehaviour, IPointerClickHandler,IDragHandler
+{
+
+    public  RectTransform Parent_rect;
+    public RectTransform cross_rect;
+    void Start()
+    {
+        //cross_rect = this.GetComponent<RectTransform>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Vector2 pos = new Vector2();
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(Parent_rect, eventData.position, eventData.enterEventCamera, out pos))
+        {
+            cross_rect.anchoredPosition = pos;
+
+        }
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos = new Vector2();
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(Parent_rect, eventData.position, eventData.enterEventCamera, out pos))
+        {
+            cross_rect.anchoredPosition = pos;
+
+        }
+    }
+}
