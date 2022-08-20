3cc6ebed40227e2f23b3f40a65a7d016ed3aef3d
behnam hesabi <atlasplants@gmail.com>
2022-08-20 19:03:25 +0430

    omid

--- Assets/Script/PlatformEffector.cs
+++ Assets/Script/PlatformEffector.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlatformEffector : MonoBehaviour
 {
     public  float PowerPlatform = 10;
+    public bool WithNormal = false;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Body")
         {
-            var dir = collision.collider.GetComponent<Body>().Lastvelocity - collision.contacts[0].point.normalized;
-           // var reflect = Vector3.Reflect(dir, collision.contacts[0].normal);
-            collision.rigidbody.AddForce(transform.up* PowerPlatform, ForceMode.Impulse);
-            Debug.Log("Effector");
+            if (!WithNormal)
+            {
+               // var dir = collision.collider.GetComponent<Body>().Lastvelocity - collision.contacts[0].point.normalized;
+                // var reflect = Vector3.Reflect(dir, collision.contacts[0].normal);
+                collision.rigidbody.AddForce(transform.up * PowerPlatform, ForceMode.Impulse);
+                Debug.Log("Effector Y Axis");
+            }
+            else
+            {
+               
+                collision.rigidbody.AddForce(collision.contacts[0].normal * PowerPlatform, ForceMode.Impulse);
+                Debug.Log("Effector Normal Axis");
+            }
         }
     }
 }
